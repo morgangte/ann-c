02e3648e7a909a51ea6877dbcf6858902c46ba36
morgangte <morgan.gillette@ecole.ensicaen.fr>
2025-02-24 12:46:02 +0100

    clearing

--- src/test.c
+++ src/test.c
@@ -7,38 +7,9 @@
 
 #define TEST_IMAGES 10
 
-#if 0
-int main() {
-    HiddenLayer hidden;
-    OutputLayer output;
-
-    load_model("hidden_layer.bin", "output_layer.bin", &hidden, &output);
-
-    int num_images, image_size, num_labels;
-    uint8_t *images = load_mnist_images("data/train-images-idx3-ubyte", &num_images, &image_size);
-    uint8_t *labels = load_mnist_labels("data/train-labels-idx1-ubyte", &num_labels);
-
-    if (num_images != num_labels) {
-        fprintf(stderr, "Number of images and labels do not match\n");
-        exit(EXIT_FAILURE);
-    }
-
-    double accuracy = calculate_accuracy(&hidden, &output, images, labels, num_images);
-    printf("Accuracy: %.2f%%\n", accuracy * 100);
-
-    for (int i = 0; i < TEST_IMAGES; i++) {
-        uint8_t recognized_digit = recognize(&hidden, &output, &images[i * INPUT_SIZE]);
-        printf("Image %d: Recognized as %d, Actual %d\n", i + 1, recognized_digit, labels[i]);
-    }
-
-    free(images);
-    free(labels);
-    return EXIT_SUCCESS;
-}
-#else
 int main(void) {
     NeuralNetwork network = neuralnetwork_create();
-    neuralnetwork_load(&network, "my_nn_model");
+    neuralnetwork_load(&network, "model/my_nn_model");
 
     uint32_t number_of_images, image_size, number_of_labels;
     uint8_t *images = load_mnist_images("data/train-images-idx3-ubyte", &number_of_images, &image_size);
@@ -64,4 +35,3 @@ int main(void) {
     free(labels);
     return EXIT_SUCCESS;
 }
-#endif
--- src/train.c
+++ src/train.c
@@ -5,30 +5,6 @@
 #include "mnist.h"
 #include "neuralnetwork.h"
 
-#if 0
-int main() {
-    int num_images, image_size, num_labels;
-    uint8_t *images = load_mnist_images("data/train-images-idx3-ubyte", &num_images, &image_size);
-    uint8_t *labels = load_mnist_labels("data/train-labels-idx1-ubyte", &num_labels);
-
-    if (num_images != num_labels) {
-        fprintf(stderr, "Number of images and labels do not match\n");
-        exit(EXIT_FAILURE);
-    }
-
-    HiddenLayer hidden;
-    OutputLayer output;
-    initialize_layer(&hidden, &output);
-
-    train(&hidden, &output, images, labels, num_images, EPOCHS);
-
-    save_model("hidden_layer.bin", "output_layer.bin", &hidden, &output);
-
-    free(images);
-    free(labels);
-    return EXIT_SUCCESS;
-}
-#else
 int main(void) {
     uint32_t number_of_images, image_size, number_of_labels;
     uint8_t *images = load_mnist_images("data/train-images-idx3-ubyte", &number_of_images, &image_size);
@@ -43,7 +19,7 @@ int main(void) {
     neuralnetwork_initialize(&network);
     neuralnetwork_train(&network, images, labels, number_of_images, NUMBER_OF_EPOCHS);
 
-    neuralnetwork_save(&network, "my_nn_model");
+    neuralnetwork_save(&network, "model/my_nn_model");
 
     double performance = neuralnetwork_benchmark(&network, images, labels, number_of_images);
     printf("Neural Network results:\n   Accuracy: %.3f%%\n", performance * 100);
@@ -59,4 +35,3 @@ int main(void) {
     free(labels);
     return EXIT_SUCCESS;
 }
-#endif
